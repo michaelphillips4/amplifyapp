0e493ea50b212317ef9b35e6f47a07a7ac180170
michaelphillips4 <31538839+michaelphillips4@users.noreply.github.com>
2021-12-08 17:15:04 +0000

    Update App.js
    Added a line
    
    Co-Authored-By: michaelphillips4 <31538839+michaelphillips4@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         <h2>Test 1</h2>
+        <hr />
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
