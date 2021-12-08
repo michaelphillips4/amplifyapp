895edc31d18a1f997c38cbe22e24843c1b254266
michaelphillips4 <31538839+michaelphillips4@users.noreply.github.com>
2021-12-08 18:45:56 +0000

    test 3
    Co-Authored-By: michaelphillips4 <31538839+michaelphillips4@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react-v1'
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
